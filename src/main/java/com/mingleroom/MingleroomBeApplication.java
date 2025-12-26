022035ab9637a1b4fe2f5032c4e83be1b6870737
OhSangH <dhtkdgus126@gmail.com>
2025-12-26 16:44:03 +0900

    feat : 20251226 폴더 구조 수정

--- src/main/java/com/mingleroom/MingleroomBeApplication.java
+++ src/main/java/com/mingleroom/MingleroomBeApplication.java
@@ -1,4 +1,4 @@
-package com.example.mingleroom_be;
+package com.mingleroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/mingleroom/MingleroomBeApplicationTests.java
+++ src/test/java/com/mingleroom/MingleroomBeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.mingleroom_be;
+package com.mingleroom;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
