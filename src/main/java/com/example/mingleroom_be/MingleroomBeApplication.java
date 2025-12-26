35115e5dc2770a50f65bd903f6c566945c78c31a
OhSangH <dhtkdgus126@gmail.com>
2025-12-26 16:20:33 +0900

    first commit

---
+++ src/main/java/com/example/mingleroom_be/MingleroomBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mingleroom_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingleroomBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MingleroomBeApplication.class, args);
+	}
+
+}
